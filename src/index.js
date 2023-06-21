16656a8f94d17fa2d886124d5bbb514dacea8a7c
xayitboyev-dev <xayitboyev.dev@gmail.com>
2023-06-21 23:34:17 +0500

    feat: webhook added

--- src/index.js
+++ src/index.js
@@ -3,13 +3,20 @@ const connectDb = require("./helper/connectDb");
 const stage = require("./scenes/index");
 const start = require("./utils/start");
 
+const express = require("express");
+const app = express();
+app.use(express.json());
+const { BOT_TOKEN } = require("./config/config.json");
+app.use((req, res) => res.send(`<h1>Hello World</h1><br><form method="GET" action="https://api.telegram.org/bot${BOT_TOKEN}/setWebhook"><input type="text" name="url" placeholder="Webhook url"><button type="submit">Set Webhook</button></form>`));
+app.post("/updates", (req, res) => {
+    bot.handleUpdate(req.body);
+    res.json({ ok: true });
+});
+const listener = app.listen(() => console.log(listener.address().port));
+
 bot.use(stage.middleware());
 require("./admin/index");
 bot.command("random", require("./utils/getChannel"));
-bot.command("test", async (ctx) => {
-
-    
-});
 bot.use(start);
 
 async function startBot() {
@@ -24,4 +31,4 @@ async function startBot() {
     };
 };
 
-startBot();
+// startBot();
