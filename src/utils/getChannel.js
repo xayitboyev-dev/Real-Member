f2b3e6f4bcb5e0db3ef4699cda31d82cef31cd92
xayitboyev-dev <xayitboyev.dev@gmail.com>
2023-07-11 18:12:25 +0500

    feat: some channels profile photo not showing error fixed

--- src/utils/getChannel.js
+++ src/utils/getChannel.js
@@ -27,7 +27,8 @@ module.exports = (ctx, msg) => new Promise((resolve) => {
             const about = await bot.telegram.getChat(order.channel);
             await bot.telegram.getChatAdministrators(order.channel);
             const text = `<b>${about.type == "channel" ? "📣 KANAL" : "👥 GURUH"}</b>\n\n<b>Nomi:</b> <i>${about.title}</i>\n<b>Username:</b> <i>@${about.username}</i>\n<b>Id:</b> <i>${about.id}</i>\n\n<b>Kanalga a'zo bo'ling va 2 ta olmos oling!</b>`;
-            resolve({ text, photo: about.photo.big_file_id, inline: taskInline(about.username, order.orderNumber, about.type) });
+            const defaultImage = "AQADAgADms0xG3cccEkACAMAA8k6pcYW____-b26RFfoJw4vBA";
+            resolve({ text, photo: about.photo?.big_file_id || defaultImage, inline: taskInline(about.username, order.orderNumber, about.type) });
         } catch (error) {
             console.log(error);
             if (error.on) {
